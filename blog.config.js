ee9346e56ae46b45d98a17c6c042470c648a0153
zhangbowen <zhangbowen.frontend@bytedance.com>
2022-09-02 01:12:11 +0800

    feat: update

--- blog.config.js
+++ blog.config.js
@@ -13,7 +13,7 @@ const BLOG = {
   since: 2022, // If leave this empty, current year will be used.
   postsPerPage: 10,
   sortByDate: true,
-  showAbout: false,
+  showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
