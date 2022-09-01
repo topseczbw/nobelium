48cd685cd629a8e5f66f38e755ac0cddf7e407cd
zhangbowen <zhangbowen.frontend@bytedance.com>
2022-09-02 02:18:09 +0800

    feat: update

--- blog.config.js
+++ blog.config.js
@@ -10,7 +10,7 @@ const BLOG = {
   lightBackground: '#f1fafa', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#181818', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2021, // If leave this empty, current year will be used.
   postsPerPage: 10,
   sortByDate: true,
   showAbout: true,
